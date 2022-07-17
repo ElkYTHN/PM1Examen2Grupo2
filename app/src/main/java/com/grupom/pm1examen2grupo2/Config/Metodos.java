a9e2ebbcd0cf643265f4c709e1e51f139c671248
ElkYTHN <elkinportillo4@gmail.com>
2022-07-16 22:47:19 -0600

    Nuevos cambios 2

--- app/src/main/java/com/grupom/pm1examen2grupo2/Config/Metodos.java
+++ app/src/main/java/com/grupom/pm1examen2grupo2/Config/Metodos.java
@@ -1,4 +1,19 @@
 package com.grupom.pm1examen2grupo2.Config;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Bitmap;
+
+import androidx.appcompat.app.AlertDialog;
+
+import java.io.ByteArrayOutputStream;
 public class Metodos {
+
+    public byte[] setBitmaptobyte(Bitmap bitmap) {
+
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+        byte[] ArrayFirma  = stream.toByteArray();
+        return ArrayFirma;
+    }
 }
--- app/src/main/java/com/grupom/pm1examen2grupo2/Config/RestApiMethods.java
+++ app/src/main/java/com/grupom/pm1examen2grupo2/Config/RestApiMethods.java
@@ -1,4 +1,17 @@
 package com.grupom.pm1examen2grupo2.Config;
 
 public class RestApiMethods {
+    private static final String ipaddress = "elkinwebhn.com";
+    public static final String StringHttp = "http://";
+    //EndPoint Urls
+    private static final String GetEmple = "/API_Examen/listacontactos.php";
+    private static final String GetBuscar = "/API_Examen/listasinglecontacto.php?nombre=";
+    private static final String setUpdate = "/API_Examen/actualizarcontacto.php";
+    private static final String CreateUsuario = "/API_Examen/crearcontacto.php";
+
+    //metodo get
+    public static final String EndPointGetContact = StringHttp + ipaddress + GetEmple;
+    public static final String EndPointGetBuscarContact = StringHttp + ipaddress + GetBuscar;
+    public static final String EndPointSetUpdateContact = StringHttp + ipaddress + setUpdate;
+    public static final String EndPointCreateUsuario = StringHttp + ipaddress + CreateUsuario;
 }
