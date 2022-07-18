7be6546ab8f3a5e33499d9671b5a77fdf08a2d9d
ElkYTHN <elkinportillo4@gmail.com>
2022-07-18 10:07:27 -0600

    Nuevos cambios 15

--- app/src/main/java/com/grupom/pm1examen2grupo2/ActualizarContactos.java
+++ app/src/main/java/com/grupom/pm1examen2grupo2/ActualizarContactos.java
@@ -2,9 +2,23 @@ package com.grupom.pm1examen2grupo2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class ActualizarContactos extends AppCompatActivity {
+
+    Button btnAtras, btnActualizar,btnGaleria;
+    FloatingActionButton btnTomarfoto;
+    EditText txtNombre,txtTelefono,txtLat,txtLon;
+    ImageView Foto;
+    String fotoString;
+
+    Bitmap imagen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
